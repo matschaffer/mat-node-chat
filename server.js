2c69c6cf853b360ff169889a95cd6d5b4d37e162
Mat Schaffer <mat.schaffer@gmail.com>
2010-06-12 00:17:08 -0400

    A basic hello world server, I hope.

---
+++ server.js
@@ -0,0 +1,9 @@
+var sys = require('sys'),
+    http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello from heroku\n');
+}).listen(8124, "127.0.0.1");
+
+sys.puts('Server running at http://127.0.0.1:8124/');
