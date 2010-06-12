904843f059e09184f24c906b05194e4a1f481046
Mat Schaffer <mat.schaffer@gmail.com>
2010-06-12 13:56:18 -0400

    Reading from a file.

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 var sys = require('sys'),
     http = require('http'),
+    fs = require('fs'),
     port = parseInt(process.env.PORT || 8000);
 
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello from heroku\n');
+    fs.readFile('index.html', function(err, data) {
+        if (err) {
+          sys.puts('Error loading file.');
+        } else {
+          sys.puts('Loading file.');
+        }
+
+        res.writeHead(200, {
+          'Content-Type':   'text/html',
+          'Content-Length': data.length
+        });
+        res.write(data);
+        res.close();
+      });
 }).listen(port);
 
 sys.puts('Server running at http://*:' + port);
