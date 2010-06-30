60e7cb996651f393641d2008e51b25d3ce71096b
Mat Schaffer <mat.schaffer@gmail.com>
2010-06-29 23:12:57 -0400

    Found it, I think.

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-var sys  = require('sys');
-    //Faye = require('./faye-node'),
-    //adapter = new Faye.NodeAdapter();
+var sys  = require('sys'),
+    Faye = require('./faye-node'),
+    adapter = new Faye.NodeAdapter();
 
-//function handleFaye(request, response, next) {
-//  if (!adapter.call(request, response)) {
-//    next();
-//  }
-//}
+function handleFaye(request, response, next) {
+  if (!adapter.call(request, response)) {
+    next();
+  }
+}
 
 module.exports = new require('./lib/connect').createServer([
   {filter: 'log'},
-  //{module: { handle: handleFaye } },
+  {module: { handle: handleFaye } },
   {provider: "static", root: "./public"}
 ]);
--- server.js
+++ server.js
@@ -1,2 +1,2 @@
-require('./app').listen(process.env.PORT || 3000);
+require('./app').listen(parseInt(process.env.PORT) || 3000);
 // db url is at process.env.DATABASE_URL
