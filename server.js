602b6579045a037c01a7573ac6d2d07e8fe90534
Mat Schaffer <mat.schaffer@gmail.com>
2010-08-27 16:14:28 -0400

    output process info

--- server.js
+++ server.js
@@ -1,2 +1,5 @@
+sys.puts("Node version is " + process.version);
+sys.puts("Env details are " + sys.inspect(process.env));
+
 require('./app').listen(parseInt(process.env.PORT) || 3000);
 // db url is at process.env.DATABASE_URL
